d183ef9fdb854907abad883b93ed28c679418b36
Stanislav Kogiku <s.kogiku@gmail.com>
2020-07-27 10:16:21 +0900

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Stanislav Kogiku on 2020/07/27.
+//  Copyright © 2020 Stanislav Kogiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
